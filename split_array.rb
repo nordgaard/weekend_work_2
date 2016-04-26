987c1de71acabd3c4260a34ece7bfbdf9f155fc0
Jens Nordgaard <jenordgaard@gmail.com>
2016-04-26 09:42:27 -0700

    Included all work for weekend 2

---
+++ split_array.rb
@@ -0,0 +1,41 @@
+# Complete the method called split_array, which two parameters, the first being an array, and the second being an integer. The method will then create an array of other arrays, each one being the size of the indicated by the second parameter. See the driver code for examples.
+
+def split_array(array, array_size)
+  big_array = []
+  while array.any?
+    sub_array = []
+    array_size.times do
+      sub_array << array.shift
+    end
+    big_array << sub_array
+  end
+  return big_array
+end
+
+# Driver code - don't touch anything below this line.
+puts "TESTING split_array..."
+puts
+
+result = split_array([0, 1, 2, 3, 4, 5], 2)
+
+puts "Your method returned:"
+p result
+puts
+
+if result == [[0, 1], [2, 3], [4, 5]]
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = split_array([0, 1, 2, 3, 4, 5], 3)
+
+puts "Your method returned:"
+p result
+puts
+
+if result == [[0, 1, 2], [3, 4, 5]]
+  puts "PASS!"
+else
+  puts "F"
+end
